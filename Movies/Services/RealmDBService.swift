df1a3e77604e7972ac4c18b54c9e9c92525b9ea3
MelnykovMykyta <melnikovnikita@ukr.net>
2024-02-04 20:31:23 +0200

    add FavoriteViewModel, MovieDBObject, RealmDBService

---
+++ Movies/Services/RealmDBService.swift
@@ -0,0 +1,8 @@
+//
+//  RealmDBService.swift
+//  Movies
+//
+//  Created by Nikita Melnikov on 04.02.2024.
+//
+
+import Foundation
---
+++ Movies/ViewModels/FavoriteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteViewModel.swift
+//  Movies
+//
+//  Created by Nikita Melnikov on 04.02.2024.
+//
+
+import Foundation
