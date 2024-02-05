53e45ef7ea51e03ff0066bf4b96f9b7381b67995
MelnykovMykyta <melnikovnikita@ukr.net>
2024-02-05 15:26:50 +0200

    finish test tasks

---
+++ Movies/Controllers/FavoriteMovieDetailsVC.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteMovieDetailsVC.swift
+//  Movies
+//
+//  Created by Nikita Melnikov on 05.02.2024.
+//
+
+import Foundation
---
+++ Movies/Views/Favorite/FavoriteDetailsTVC.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteDetailsTVC.swift
+//  Movies
+//
+//  Created by Nikita Melnikov on 05.02.2024.
+//
+
+import Foundation
